e47efa967dbeeb705716ad3751dbe8fa40fb8834
m4rkbello <markbello10041999@gmail.com>
2025-12-21 23:11:07 +0800

    TUG NA YAWAS!

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
+import 'react-native-gesture-handler'; // ⚠️ MUST be at the very top
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src/App';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
