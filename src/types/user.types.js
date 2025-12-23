d5932fb138c72af0dd65920f6136f7ff13090424
m4rkbello <markbello10041999@gmail.com>
2025-12-23 23:08:47 +0800

    ADDED

---
+++ src/types/user.types.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const user.types = () => {
+  return (
+    <View>
+      <Text>user.types</Text>
+    </View>
+  )
+}
+
+export default user.types
