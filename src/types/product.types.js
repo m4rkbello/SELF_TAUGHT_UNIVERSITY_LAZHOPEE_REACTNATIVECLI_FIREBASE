b141f4d44541ac661b2995ae08462279f68be919
m4rkbello <markbello10041999@gmail.com>
2025-12-23 22:12:44 +0800

    TUG NA YAWAS!

---
+++ src/types/product.types.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const product.types = () => {
+  return (
+    <View>
+      <Text>product.types</Text>
+    </View>
+  )
+}
+
+export default product.types
+
+const styles = StyleSheet.create({})
